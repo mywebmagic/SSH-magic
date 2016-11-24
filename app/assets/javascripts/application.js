7422323d71cc2e6a8573c48149147b9eaf2b9cde
Nicholas Goosen <ntgoosen@gmail.com>
2016-11-24 13:46:41 +0000

    Updated forms for stripe

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,7 +3,7 @@
 //
 // Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
 // or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-//
+// 
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // compiled file.
 //
@@ -13,5 +13,4 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-sprockets
-//= require turbolinks
 //= require_tree .
